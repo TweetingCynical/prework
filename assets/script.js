5ea5c845e086776b3d491fbc00e5e0446e05709d
Jon Garnett-Smith <jon@exce-ed.com>
2023-11-25 14:46:39 +0000

    Add functions and logic for displaying messages in console. Use ES6.

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Define array of topics
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+// Use math random to choose a random item from the topics array
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+// Log each topic from topics to the console
+let listTopics = () => {
+  topics.forEach((topic) => {
+    console.log(topic);
+  });
+};
+
+// Create the function for returning a selected topic
+let selectedTopic = () =>
+  !randomTopic ? `Please try again!` : `Let's study ${randomTopic}`;
+
+// Display console messages
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+
+console.log("Which topic should we study first?");
+console.log(selectedTopic());
